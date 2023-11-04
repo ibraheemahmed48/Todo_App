552171e91ae7c96ed4a6efc55ed462c81a1620f3
Ibraheem Ahmed <ibraheemahmed836@gmail.com>
2023-11-05 02:07:13 +0300

    Improvements

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import { useRef, useState } from "react";
 import "./App.css";
+import Card from "./itme";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    { completed: false, text: "Task 1" },
+    { completed: true, text: "Task 2" },
+  ]);
   const inputRef = useRef();
 
   const handleAddTodo = () => {
     const text = inputRef.current.value;
-    if(inputRef.current.value ===""){
-      alert('The text is Empty , Enter Valid Task');
-    }else{
+    if (inputRef.current.value === "") {
+      alert("The text is Empty , Enter Valid Task");
+    } else {
       const newItem = { completed: false, text };
       setTodos([...todos, newItem]);
       inputRef.current.value = "";
     }
-    
   };
 
   const handleItemDone = (index) => {
@@ -25,35 +28,44 @@ function App() {
 
   const handleDeleteItem = (index) => {
     const newTodos = [...todos];
-    newTodos.splice(index, 1)
-    setTodos(newTodos)
-  }
-
-
-
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
 
   return (
     <div className="App">
-      <h2 className="title">To Do List</h2>
-      <div className={"to-do-container"}>
-        <ul>
-          {todos.map(({ text, completed }, index) => {
-            return (
-              <div className="item">
-                <li
-                  className={completed ? "done" : ""}
-                  key={index}
-                  onClick={() => handleItemDone(index)}
-                >
-                  {completed===false? text:text} 
-                </li>
-                <span onClick={() => handleDeleteItem(index)} className="trash">❌</span>
-              </div>
-            );
-          })}
-        </ul>
-        <input ref={inputRef} placeholder="Enter item..." />
-        <button onClick={handleAddTodo}>Add</button>
+      <h2 className="title">To Do App</h2>
+
+      <h3 className="title">TO DO LIST</h3>
+
+      <div>
+        {todos.map(({ text, completed }, index) => {
+          return (
+            <>
+              <Card
+                text={text}
+                buttonText="❌"
+                onClick={() => handleDeleteItem(index)}
+                key={index}
+                buttonTextMark={completed?"✅":"❎"}
+                onClickDone={() => handleItemDone(index)}
+              />
+            </>
+          );
+        })}
+      </div>
+
+      <div className="">
+        <div className={"to-do-container"}>
+          <input
+            ref={inputRef}
+            placeholder="Write The Task..."
+            className="input-style"
+          />
+          <button onClick={handleAddTodo} className="button-style">
+            Add
+          </button>
+        </div>
       </div>
     </div>
   );
